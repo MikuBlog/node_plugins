62752cac178fce30eb425e7cbb2eafb52799bc96
Miku <43233731+MikuBlog@users.noreply.github.com>
2019-02-06 21:33:21 +0800

    Update update_file_name.js

--- plugins/update_file_name.js
+++ plugins/update_file_name.js
@@ -13,7 +13,6 @@ function updateFileName(path, fileName, suffix, initialIndex = 0) {
         fileNames.forEach((value, index) => {
             const oldPath = path + "/" + value
             const newPath = path + "/" + fileName + (index + initialIndex) + "." + suffix
-
             fs.rename(oldPath, newPath, (err) => {
                 if(err) {
                     throw new Error(err)
